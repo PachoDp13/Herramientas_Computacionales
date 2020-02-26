286a923c1d8cc4eeb60bedf7071e971ab9639365
PachoDp13 <61482440+PachoDp13@users.noreply.github.com>
2020-02-26 11:38:57 -0500

    Add files via upload

---
+++ EjercicioSemana6FranciscoDiaz201912252.py
@@ -0,0 +1,103 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+           
+
+
+# In[9]:
+
+
+print ("Primera parte, función factorial:")
+
+def factorial(n):
+    if n==0:
+        return 1
+    else:
+        return (n*factorial(n-1))
+    
+
+for i in range (1,6):
+    print ("El factorial de",i,"es:", factorial(i) ) 
+
+print ("Segunda parte, secuencia de Fibonacci:")
+
+def fibonacci(n):
+    if n < 2:
+        return n
+    else:
+        return fibonacci(n-1) + fibonacci(n-2)
+for i in range (1,6):
+    print ("El término en la posición",i,"de Fibonacci es:", fibonacci(i)) 
+    
+print ("Tercera parte, pasar a base k:")
+
+def basek(n, k):
+    conversion = ''
+    if(n<k):
+        return str(n)
+    else:
+        return str(basek(n//k,k))+ str(n%k)    
+for i in range (1,6):
+    print ("El número",i, "en base 3 es:" ,basek(i,3))
+    
+print ("Cuarta parte, algoritmo de multiplicación:")
+
+def prod(m,n):
+    if(m==1):
+        return n
+    elif ((m%2)==0):
+        return prod(m/2,2*n)
+    elif (m>1):
+        return (prod((m-1)/2, 2*n) + n)
+m,n= 23,37
+a,b=m,n
+
+
+while (m>=1):
+    if((m%2)==0):
+        estado= "par"
+    else:
+        estado = "impar"
+    print("El valor de m es",m ,"el de n es", n, "y m es",estado)
+    if(m!=2):
+        m=(m-1)/2
+    else:
+        m=m-1
+    n=n*2
+print ("El resultado de",a,"x",b,"es:",prod(a,b))
+       
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
